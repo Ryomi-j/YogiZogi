f49122410e488210f9b6f6df56bf7d3f97ded936
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-19 15:04:57 +0900

    :sparkles:  feat: make accommodation comparison mock data

---
+++ src/mocks/api/data/accommodationComparison.ts
@@ -0,0 +1,343 @@
+export const accommodationComparisonData = [
+  {
+    id: 5,
+    accommodationName: '역삼 인트로호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/72748/479210/ab94afc451c0ce2b72d429d267e60525.jpg',
+    address: '서울 강남구 역삼동 677-14',
+    rate: 9.9,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 1,
+    accommodationName: '강남 더 뮤즈',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/1824/182031/f548520417580511897f4c4daeec7cc8.jpg',
+    address: '서울 강남구 역삼동 678-9',
+    rate: 9.8,
+    price: 40000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 13,
+    accommodationName: '역삼 브라운도트',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/692/452910/f8449703e551142be5d574571aaf68e9.jpg',
+    address: '서울 강남구 역삼동 677-16',
+    rate: 9.7,
+    price: 35000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 18,
+    accommodationName: '역삼 마리호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/67576/429720/7cc033e1c656374598ab1ad6fb65ff05.jpg',
+    address: '서울 강남구 역삼동 720-14',
+    rate: 9.7,
+    price: 500000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 31,
+    accommodationName: '역삼 사월호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/67575/364937/192d5d410e6337f7695ac1dc5bc58db9.jpg',
+    address: '서울 강남구 역삼동 720-12',
+    rate: 9.6,
+    price: 95000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 30,
+    accommodationName: '역삼 벤',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/688/445838/66ce227678d2fc88807bd8d563ac9ead.jpg',
+    address: '서울 강남구 역삼동 718-28',
+    rate: 9.6,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 89,
+    accommodationName: '강남 타라',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/1588/485920/a623ba15611e9427e7d35af4b8d74d71.JPG',
+    address: '서울 강남구 삼성동 142-19',
+    rate: 9.6,
+    price: 40000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 6,
+    accommodationName: '역삼 PREMIER XYM',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/5803/36302/abefe5e5cfe982a83cab428cdc824be7.jpg',
+    address: '서울 강남구 역삼동 718-18',
+    rate: 9.5,
+    price: 25000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 51,
+    accommodationName: '선릉 그레이호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/63362/280325/387391c080114fd4ce88ebc390393dfe.jpg',
+    address: '서울 강남구 대치동 890-28',
+    rate: 9.5,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 8,
+    accommodationName: '강남 봄',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/71693/460761/84a9ded61bba5208c9ca51ea775716c3.jpg',
+    address: '서울 서초구 반포동 748',
+    rate: 9.5,
+    price: 40000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 32,
+    accommodationName: '역삼 CF호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/11995/396142/1185a79d1a9dbb0fd95a35a63c39ae09.jpg',
+    address: '서울 강남구 역삼동 720-33',
+    rate: 9.5,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 27,
+    accommodationName: '강남 멜리샤호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/63624/281556/1be6d279cb0dc33457eaa3fac97a788d.jpg',
+    address: '서울 강남구 역삼동 825-29',
+    rate: 9.5,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 25,
+    accommodationName: '역삼 H Avenue-역삼점',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/71160/477852/ba2d96c4509ed5a9fe8f58dbfe081228.jpg',
+    address: '서울 강남구 역삼동 678-14',
+    rate: 9.4,
+    price: 25000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 90,
+    accommodationName: '역삼 아마레',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/73384/491291/e84b74b8f55436c93ae0c9c75c34c80c.jpg',
+    address: '서울 강남구 역삼동 776-19',
+    rate: 9.4,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 88,
+    accommodationName: '삼성 캘리포니아',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/6084/489669/bd2e642b4248e1b8ce7d4c1e888329d7.jpg',
+    address: '서울 강남구 삼성동 142-12',
+    rate: 9.4,
+    price: 35000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 24,
+    accommodationName: '강남 제리스플래닛',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/4541/158693/37a36175d5bb220a8abb064f7849b7d8.jpg',
+    address: '서울 강남구 역삼동 819-9',
+    rate: 9.4,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 2,
+    accommodationName: '역삼 호텔 디 아티스트',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/5675/77695/2e5be50fdaeaf537f9ee544b6a621158.jpg',
+    address: '서울 강남구 역삼동 678-13',
+    rate: 9.3,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 19,
+    accommodationName: '삼성 JS',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/4291/24038/5546a0893417ff299a137a8051e4ed3c.jpg',
+    address: '서울 강남구 삼성동 142-13',
+    rate: 9.3,
+    price: 40000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 3,
+    accommodationName: '역삼 리치웰',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/3805/22612/4f17fa78f58cba0f79dfbf12115a3c9b.jpg',
+    address: '서울 강남구 역삼동 677-9',
+    rate: 9.3,
+    price: 29800,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 21,
+    accommodationName: '대치 컬리넌',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/5625/34459/7afe31b21aeb886d02e189f9dd559b45.jpg',
+    address: '서울 강남구 대치동 891-35',
+    rate: 9.3,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 4,
+    accommodationName: '강남 캠퍼스',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/4988/31328/e13e9ca5a6796ccd14841168f0de4b64.jpg',
+    address: '서울 강남구 역삼동 825-30',
+    rate: 9.3,
+    price: 40000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 85,
+    accommodationName: '강남 블랑',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/4627/503677/77fa8c795e0954ef1808c3dff64b81b4.jpg',
+    address: '서울 강남구 역삼동 700-27',
+    rate: 9.3,
+    price: 20000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 7,
+    accommodationName: '역삼 컬리넌',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/2084/13542/f830edbc9f2e3a24be580fa1f15eb739.jpg',
+    address: '서울 강남구 역삼동 720-23',
+    rate: 9.3,
+    price: 29800,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 12,
+    accommodationName: '강남 녹스 호텔',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/891/2747/85f56df24bdb6f8e7e105b63b7265314.jpg',
+    address: '서울 강남구 역삼동 677-13',
+    rate: 9.3,
+    price: 25000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 28,
+    accommodationName: '역삼 스타 프리미어',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/6235/79301/a804ea650d7f331644a2b39a7fc1a496.jpg',
+    address: '서울 강남구 역삼동 677-19',
+    rate: 9.3,
+    price: 25000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 35,
+    accommodationName: '삼성 라엠-LaM',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/66476/338236/6818f41d543912a308037218ff732090.jpg',
+    address: '서울 강남구 삼성동 100-3',
+    rate: 9.3,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 33,
+    accommodationName: '선릉 호텔 스타',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/55443/189108/e02953d87a1a301fefdaabb004512170.jpg',
+    address: '서울 강남구 삼성동 141-21',
+    rate: 9.2,
+    price: 30000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 83,
+    accommodationName: '논현 올인',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/4669/246317/3d0365ef155b229dc34f0dfed4a58808.jpg',
+    address: '서울 강남구 논현동 118-7',
+    rate: 9.2,
+    price: 35000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 37,
+    accommodationName: '개포 컬리넌',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/46529/126099/ec71477d8e975c8e08db5d4bd1b8f204.jpg',
+    address: '서울 강남구 개포동 1229-8',
+    rate: 9.2,
+    price: 35000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 17,
+    accommodationName: '강남 카파쓰',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/4973/5105/27979e4e4f66f72ca7a9fc405fd990a9.jpg',
+    address: '서울 강남구 역삼동 719-20',
+    rate: 9.2,
+    price: 48800,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  },
+  {
+    id: 9,
+    accommodationName: '강남 더 디자이너스 리즈 프리미어',
+    picUrl:
+      'https://image.goodchoice.kr/resize_1000X500x0/adimg_new/3845/22536/06.jpg',
+    address: '서울 강남구 논현동 201-11',
+    rate: 9.2,
+    price: 35000,
+    convenience:
+      '수영장, 족구장, 와이파이, 전기밥솥, 무료주차, BBQ, 주차장, TV, 에어컨, 객실샤워실, 금연, 객실내취사'
+  }
+];
